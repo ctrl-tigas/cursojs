3a075e2c2e846db57fd161ba0cf8a84b179a4934
iroh <macedocesar.tiago@gmail.com>
2025-09-02 19:51:21 -0300

    tipos de dados.

--- bloco1-fundamentos1/script.js
+++ bloco1-fundamentos1/script.js
@@ -5,8 +5,22 @@
 // let primeiro_nome = "Tiago";
 // console.log(primeiro_nome);
 
-//Exercício 1
+/*
+Tipos de dados
+1. Number (ponto flutuante e inteiro)
+2. String (sequencia de caracteres)
+3. Boolean (verdadeiro ou falso)
+4. Undefined (valor vazio, utilizado em variáveis declaradas mas não definidas)
+5. Null (similar à undefined)
+6. Symbol (ES2015 valor único e imutável usado como identificador par aevitar colisões de nomes em propriedades de objetos)
+7. BigInt (ES2020 números maiores que tipo Number não sustenta)
+
+OBS: JavaScript é uma linguagem de tipagem dinânimca (tipo da variável é atribuído automaticamente)
+*/
+
+/*Exercício 1
 let pais = "Brasil";
 let continente = "America do Sul";
 let populacao = 212;
 console.log(`País: ${pais}\nContinente: ${continente}\nPopulação: ${populacao} milhões`);
+*/
