489a321d72697916a75e683fd7b70dc503409c62
iroh <macedocesar.tiago@gmail.com>
2025-09-01 21:45:47 -0300

    declaração de variáveis. exercício 1

--- bloco1-fundamentos1/script.js
+++ bloco1-fundamentos1/script.js
@@ -1,3 +1,12 @@
 // let js = "amazing";
 // if (js === "amazing") alert("JavaScript is fun!");
-console.log(40 + 8 + 23 - 10);
+// console.log(40 + 8 + 23 - 10);
+
+// let primeiro_nome = "Tiago";
+// console.log(primeiro_nome);
+
+//Exercício 1
+let pais = "Brasil";
+let continente = "America do Sul";
+let populacao = 212;
+console.log(`País: ${pais}\nContinente: ${continente}\nPopulação: ${populacao} milhões`);
